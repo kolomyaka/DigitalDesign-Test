2c90e048c6fb09dd09a2151f6fb7db09d313f914
kolomyaka1 <66018338+kolomyaka1@users.noreply.github.com>
2022-03-02 01:07:52 +0300

    Code refactoring

--- index.js
+++ index.js
@@ -11,18 +11,16 @@ const getDayInfo = (date) => {
     let currentMonthOfYear = new Date(info).toLocaleString('ru', {month: 'long'}); // Месяц
     let currentYearOfDate = new Date(info).toLocaleString('ru', {year: 'numeric'}); // Год
 
-
-    if (info[0] == 8 || info[0] == 3) {
+    if (info[0] == 8 || info[0] == 3) {  // Проверяем месяц для склонения
         currentMonthOfYear = currentMonthOfYear.replace(/.$/, 'та');
     } else {
         currentMonthOfYear = currentMonthOfYear.replace(/.$/, 'я');
     }
-      // 
 
     let DayOfWeek = currentDayOfWeek[0].toUpperCase() + currentDayOfWeek.slice(1);
     let MonthOfYear = currentMonthOfYear[0].toUpperCase() + currentMonthOfYear.slice(1);
 
-    const numberOfWeek = (day) => {    
+    const numberOfWeek = (day) => {  // Определяем номер недели в зависимости от дня
         if (day > 0 && day <= 7) {
             return 1;
         } else if (day > 7 && day <= 14) {
@@ -32,10 +30,11 @@ const getDayInfo = (date) => {
         } else if (day > 21) {
             return 4;
         }
-    }
-    let numberWeek = numberOfWeek(info[1]);
+    };
+
+    let numberWeek = numberOfWeek(info[1]);  
     
-    return DayOfWeek + ',' + numberWeek + ' неделя ' + MonthOfYear + ' ' + currentYearOfDate + " года";
+    return console.log(DayOfWeek + ',' + numberWeek + ' неделя ' + MonthOfYear + ' ' + currentYearOfDate + " года");
 }
 
-console.log(getDayInfo('21.03.2022'));
+getDayInfo('11.03.2022');
